5a93313cdbf50d17b4b1bc3368bee88abb9737bd
Triketek <100535116+Triketek@users.noreply.github.com>
2023-09-05 13:58:50 -0300

    ahahahahah8

---
+++ Source/cementery/cementeryGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "cementeryGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CEMENTERY_API AcementeryGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
